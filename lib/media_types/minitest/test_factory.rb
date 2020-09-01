cd5a5d9b7dc1b2e9dc215cf14070ab76acea1f0e
root <root@louis.dev.delftsolutions.nl>
2020-09-01 07:23:28 +0200

    Refactors and simplifies build_fixture_test

--- lib/media_types/minitest/test_factory.rb
+++ lib/media_types/minitest/test_factory.rb
@@ -4,18 +4,15 @@ class Minitest::Test < Minitest::Runnable
   include MediaTypes::Assertions
   def self.build_fixture_tests(type)
     scheme = type.media_type_validations.scheme
-    counter = 1
-    scheme.fixtures.each do |object|
-      json = JSON.parse(object.fixture, { symbolize_names: true })
-      caller = object.caller
-      test_type = object.expect_to_pass ? 'assert_pass' : 'assert_fail'
-      string = "test_fixture#{counter}_#{test_type}_for_#{type.to_constructable})"
-      output = object.expect_to_pass ? scheme.process_assert_pass(json, caller) : scheme.process_assert_fail(json, caller)
-      message = object.expect_to_pass ? output : MediaTypes::MediaTypeValidationError.new(json, caller).message
-      define_method string do
-        traceless_assert output.nil?, message
+
+    scheme.fixtures.each_with_index do |fixture_data, counter|
+      json = JSON.parse(fixture_data.fixture, { symbolize_names: true })
+      test_name = "test_fixture#{counter}_#{fixture_data.expect_to_pass ? 'assert_pass' : 'assert_fail'}_for_#{type.to_constructable})"
+
+      define_method test_name do
+        processed = fixture_data.expect_to_pass ? scheme.process_assert_pass(json, fixture_data.caller) : scheme.process_assert_fail(json, fixture_data.caller)
+        traceless_assert processed.nil?,  fixture_data.expect_to_pass ? processed : MediaTypes::MediaTypeValidationError.new(json, fixture_data.caller).message
       end
-      counter += 1
     end
   end
 end
