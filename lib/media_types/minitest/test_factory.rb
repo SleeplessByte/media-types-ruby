d7e808512710abd65b24767331ff1ab61f8922f0
root <root@louis.dev.delftsolutions.nl>
2020-12-03 18:07:31 +0100

    WIP: Add test factory partial refactor

--- lib/media_types/minitest/test_factory.rb
+++ lib/media_types/minitest/test_factory.rb
@@ -8,13 +8,23 @@ class Minitest::Test < Minitest::Runnable
     scheme.fixtures.each_with_index do |fixture_data, counter|
       json = JSON.parse(fixture_data.fixture, { symbolize_names: true })
       test_name = "test_fixture#{counter}_#{fixture_data.expect_to_pass ? 'assert_pass' : 'assert_fail'}_for_#{type.to_constructable})"
-      define_method test_name do
-        processed = fixture_data.expect_to_pass ? scheme.process_assert_pass(json, fixture_data.caller) : scheme.process_assert_fail(json, fixture_data.caller)
-        msg = fixture_data.expect_to_pass ? processed : MediaTypes::MediaTypeValidationError.new(json, fixture_data.caller).message
 
-        assert true
-        raise Minitest::Assertion, msg, '' unless processed.nil?
+      generate_test(test_name, fixture_data, scheme, json)
+    end
+  end
+
+  def generate_test(test_name, fixture_data, scheme, json)
+    define_method test_name do
+      if fixture_data.expect_to_pass
+        processed = scheme.process_assert_pass(json, fixture_data.caller)
+        msg = processed
+      else
+        processed = scheme.process_assert_fail(json, fixture_data.caller)
+        msg = MediaTypes::MediaTypeValidationError.new(json, fixture_data.caller).message
       end
+
+      assert true
+      raise Minitest::Assertion, msg, '' unless processed.nil?
     end
   end
 end
