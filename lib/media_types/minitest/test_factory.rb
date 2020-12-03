caccef8107898d15d284bdb133ad05addca4b7c7
root <root@louis.dev.delftsolutions.nl>
2020-12-03 17:28:52 +0100

    Refactors test_factory to make traceless assert redundant and removes traceless assert

--- lib/media_types/minitest/test_factory.rb
+++ lib/media_types/minitest/test_factory.rb
@@ -8,10 +8,12 @@ class Minitest::Test < Minitest::Runnable
     scheme.fixtures.each_with_index do |fixture_data, counter|
       json = JSON.parse(fixture_data.fixture, { symbolize_names: true })
       test_name = "test_fixture#{counter}_#{fixture_data.expect_to_pass ? 'assert_pass' : 'assert_fail'}_for_#{type.to_constructable})"
-
       define_method test_name do
         processed = fixture_data.expect_to_pass ? scheme.process_assert_pass(json, fixture_data.caller) : scheme.process_assert_fail(json, fixture_data.caller)
-        traceless_assert processed.nil?,  fixture_data.expect_to_pass ? processed : MediaTypes::MediaTypeValidationError.new(json, fixture_data.caller).message
+        msg = fixture_data.expect_to_pass ? processed : MediaTypes::MediaTypeValidationError.new(json, fixture_data.caller).message
+
+        assert true
+        raise Minitest::Assertion, msg, '' unless processed.nil?
       end
     end
   end
