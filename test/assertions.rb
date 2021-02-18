a2aa0a52596b84029cfd4de824f0f130f094ebbd
Vincent Robbemond <vrobbemond@protonmail.com>
2021-02-18 17:22:56 +0100

    make folder structure reflect modules

--- test/assertions.rb
+++ test/assertions.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-require 'media_types/minitest/assert_media_type_format'
-require 'media_types/minitest/assert_mediatype'
+require 'media_types/assertions/assert_media_type_format'
+require 'media_types/assertions/assert_mediatype'
