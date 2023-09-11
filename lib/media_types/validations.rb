2a8a68bf30b43f0803f6bf582c6b38bc81d21851
Max Maton <info@maxmaton.nl>
2023-09-12 00:27:03 +0200

    Fix crash when defining multiple versions (#151)
    * Fix crash when defining multiple validation versions
    
    * Add versions test
    
    * Run in correct scope
    
    * Fix missing method in test

--- lib/media_types/validations.rb
+++ lib/media_types/validations.rb
@@ -97,7 +97,7 @@ module MediaTypes
     def versions(versions, &block)
       versions.each do |v|
         Validations.new(media_type.version(v), registry) do
-          block(v)
+          instance_exec(v, &block)
         end
       end
     end
